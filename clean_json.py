5305b5d44c48147efcfdccb713fa0b652cdcecc6
christiankaylanUW <ckalviz@uw.edu>
2026-07-01 09:52:11 -0700

    small fixes to cleaning script

--- clean_json.py
+++ clean_json.py
@@ -12,19 +12,17 @@ with open(input_file, “r”, encoding=”utf-8”) as f:
 cleaned_transcripts = [] 				# initialize empty list of dictionaries
 
 for item in transcripts:
-title = item["title"]
-bib_number = item["field_bib_number"]
-body = item["body"]
-
-interviewee = name_pattern.search(title).group(1).strip()	# group(1) is first occurrence
-
-cleaned_transcripts.append({
-        "field_bib_number": bib_number,
-        "interviewee": interviewee,
-        "body": body
-})
+        title = item["title"]
+        bib_number = item["field_bib_number"]
+        body = item["body"]
+        interviewee = name_pattern.search(title).group(1).strip()	# group(1) is first occurrence
+        cleaned_transcripts.append({
+                "field_bib_number": bib_number,
+                "interviewee": interviewee,
+                "body": body
+        })
 
 with open(output_file, "w", encoding="utf-8") as f:
-json.dump(cleaned_transcripts, f, indent=2, ensure_ascii=False)
+        json.dump(cleaned_transcripts, f, indent=2, ensure_ascii=False)
 
 
