c2ec57decd83ddcd8eddc578efce9123e2b11d48
christiankaylanUW <ckalviz@uw.edu>
2026-07-14 12:57:43 -0700

    create bar charts for zero-shot images

---
+++ zeroshot_charts.R
@@ -0,0 +1,27 @@
+####
+# zeroshot_charts.R
+# Create bar charts for visualization of how zeroshot labels performs/labels on full image dataset. 
+###
+library(ggplot2)
+library(forcats)
+library(dplyr)
+library(jsonlite)
+
+
+# Input json file from get_concept_labels.py
+data <- fromJSON("INPUT_FILE_HERE_HERE.json")
+
+# Aggregate/count the number of images categorized as each label 
+data_counts <- data %>%
+  count(assigned_label, name = "count") %>%
+  arrange(desc(count))
+
+# create bar chart of counts
+data_counts %>% 
+  mutate(assigned_label = fct_reorder(assigned_label, count, .desc = TRUE)) %>% 
+  ggplot(aes(x = assigned_label, y = count, fill = count)) + 
+  geom_col() + 
+  scale_fill_gradient(low = "mediumpurple1", high = "mediumpurple4") + 
+  labs(x = "Entity Type", y = "Counts", fill = "Counts") +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1)) + 
+  geom_text(aes(label = count), vjust = -0.5, size = 3.5)
